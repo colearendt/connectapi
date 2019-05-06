2b63a321227be2adcf3ce4893a9d278e627aa193
Cole Arendt <cole.arendt@outlook.com>
2019-05-06 15:34:56 -0400

    improve data_pool testing

--- tests/testthat/test-data_pool.R
+++ tests/testthat/test-data_pool.R
@@ -1,5 +1,9 @@
 context("add_data_pool")
 
+test_that("fails for non data.frame", {
+  expect_error(add_data_pool(list(1,2,3)))
+})
+
 test_that("preserves metadata", {
   test_existing <- "test1234"
   rmarkdown::output_metadata$set(rsc_output_files = test_existing)
@@ -37,3 +41,35 @@ test_that("does not duplicate records", {
   expect_identical(first_metadata, second_metadata)
 })
 
+test_that("writes json output", {
+  expect_true(file.exists("connectapi_data_pool.json"))
+})
+
+test_that("json output has attributes", {
+  start_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  tmp_json_attribute <- data.frame(a=c(1,2,3,4))
+  res <- add_data_pool(tmp_json_attribute)
+  
+  expect_named(
+    res$tmp_json_attribute, 
+    c("filename", "size", "format", "nrow", "ncol", "colname", "coltype", "colclass")
+  )
+})
+
+test_that("json output is an output file", {
+  start_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  
+  tmp_json_output <- data.frame(a=c(1,2,3))
+  add_data_pool(tmp_json_output)
+  first_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  
+  expect_true("connectapi_data_pool.json" %in% first_metadata)
+})
+
+test_that("specified name is honored", {
+  skip("not implemented")
+})
+
+test_that("specified filename is honored", {
+  skip("not implemented")
+})
