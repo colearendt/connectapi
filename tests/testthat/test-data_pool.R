02149773aa99c0212956ee71f2df829f7c909e5e
Cole Arendt <cole.arendt@outlook.com>
2019-05-06 13:55:47 -0400

    add first tests for data_pool

---
+++ tests/testthat/test-data_pool.R
@@ -0,0 +1,39 @@
+context("add_data_pool")
+
+test_that("preserves metadata", {
+  test_existing <- "test1234"
+  rmarkdown::output_metadata$set(rsc_output_files = test_existing)
+  
+  start_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  
+  tmp_preserve <- data.frame(a = c(1,2,3), b = c(4,5,6))
+  add_data_pool(tmp_preserve)
+  
+  first_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  expect_true(test_existing %in% first_metadata)
+})
+
+test_that("writes feather by default", {
+  tmp_feather <- data.frame(a = c(1,2,3), b = c(4,5,6))
+  add_data_pool(tmp_feather)
+  first_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  
+  expect_true("tmp_feather.feather" %in% first_metadata)
+  expect_true(file.exists("tmp_feather.feather"))
+})
+
+test_that("does not duplicate records", {
+  start_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  
+  tmp_data <- data.frame(a = c(1,2,3), b = c(4,5,6))
+  add_data_pool(tmp_data)
+  first_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  
+  expect_true("tmp_data.feather" %in% first_metadata)
+  
+  add_data_pool(tmp_data)
+  second_metadata <- rmarkdown::output_metadata$get("rsc_output_files")
+  
+  expect_identical(first_metadata, second_metadata)
+})
+
